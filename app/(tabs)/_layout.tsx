3b08de78f79e9865d2b7eb6aaa3fb3b7202436f9
Rodrigo Ramirez <rodrigo.empoleon@gmail.com>
2026-06-09 15:02:57 -0600

    fixes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,7 @@
 import {Tabs} from "expo-router";
 import {tabs} from "@/constants/data";
-import {View} from "react-native";
+import {View,Image} from "react-native";
 import clsx from "clsx";
-import {Image} from "react-native";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {colors, components} from "@/constants/theme";
 
